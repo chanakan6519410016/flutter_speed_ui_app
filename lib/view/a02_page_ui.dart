4d5926fb5590147ba91fa021e8ca3f0364c58e1c
chanakan6519410016 <s6519410016@sau.ac.th>
2025-02-15 16:52:00 +0700

    V2 home/a01/a02

--- lib/view/a02_page_ui.dart
+++ lib/view/a02_page_ui.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class A02PageUi extends StatefulWidget {
@@ -17,19 +19,108 @@ class _A02PageUiState extends State<A02PageUi> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            // Welcome Back
             Text(
               'Welcome Back',
               style: TextStyle(
-                textAlign: TextAlign.center,
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
               ),
             ),
             SizedBox(height: 10),
             Text(
-              'Lorem ipsum do',
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+              'Diam maecenas mi non sed ut odio. Non, justo, sed facilisi et.',
               textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15,
+                color: const Color.fromARGB(255, 0, 0, 0),
+              ),
             ),
+            SizedBox(height: 30),
+            // 'Username , Email & Phone Num,ber'
+            TextField(
+              style: TextStyle(
+                fontSize: 13,
+              ),
+              decoration: InputDecoration(
+                hintText: 'Username , Email & Phone Num,ber',
+                filled: true,
+                fillColor: Colors.grey.shade100,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+            SizedBox(height: 15),
+            // 'Password'
+            TextField(
+              style: TextStyle(
+                fontSize: 13,
+              ),
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Password',
+                filled: true,
+                fillColor: Colors.grey.shade100,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            // 'Forgot Password?'
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Forgot Password?',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ),
+            SizedBox(height: 15),
+            // sing ln Button
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 243, 154, 216),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                ),
+                child: Text(
+                  "Sign in",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ),
+            SizedBox(height: 15),
+            //'Or Sign up With'
+            Row(
+              children: [
+                Expanded(
+                  child: Divider(
+                      thickness: 1,
+                      color: const Color.fromARGB(255, 240, 126, 164)),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Text('Or Sigh up With'),
+                ),
+                Expanded(
+                  child: Divider(
+                      thickness: 1,
+                      color: const Color.fromARGB(255, 223, 161, 181)),
+                ),
+              ],
+            )
           ],
         ),
       ),
